cbfb370561ca4224a360ebc1853182e24dd95424
Timofey Safronov <44961856+VanClifff@users.noreply.github.com>
2024-07-14 01:50:28 +0300

    initialization

---
+++ src/test/java/com/safronov/timofei/telegram/helper/bot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safronov.timofei.telegram.helper.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
